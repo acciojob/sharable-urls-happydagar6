4f0f537529995ed1ba296f7690e5bbfc8d2a9f23
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-03-20 19:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+
+const nameInput = document.getElementById("name");
+const yearInput = document.getElementById("year");
+const button = document.getElementById("button");
+const urlDisplay = document.getElementById("url");
+
+button.addEventListener("click", (event) => {
+	event.preventDefault();
+	const name = nameInput.value;
+	const year = yearInput.value;
+
+	let queryString = `?name=${name}&year=${year}`; // Create a query string using template literals that includes the name and year parameters
+    let fullURL = `https://localhost:8080/${queryString}`;
+
+	urlDisplay.textContent = fullURL;
+})
